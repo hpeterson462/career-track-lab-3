09b96313362131db497f2e771f14ec81e69990cb
Heather Peterson <hpeterson462@gmail.com>
2020-09-08 21:00:25 -0700

    post route passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,20 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Bird = require('../lib/models/bird');
 
-describe('alchemy-app routes', () => {
+describe('bird routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+  });
+
+  it('create a new bird via POST', async () => {
+    const response = await request(app)
+      .post('/api/v1/birds')
+      .send({
+        name: 'red-tailed hawk',
+        type: 'hawk',
+        flies: true
+      });
   });
 });
